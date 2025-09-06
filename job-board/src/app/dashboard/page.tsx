4cfd7b495a71846e78378bb2be3d320d4f7120bb
Joseph <jotienoochieng@gmail.com>
2025-09-07 01:13:41 +0300

    sign and login pages

---
+++ job-board/src/app/dashboard/page.tsx
@@ -0,0 +1,32 @@
+"use client";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import React from "react";
+
+const DashBoard = () => {
+  const router = useRouter();
+  const onLogout = async () => {
+    try {
+      const response = await axios.get("/api/users/logout");
+      console.log("logout success", response.data);
+      router.push("/login");
+    } catch (error: any) {
+      console.log("Logout failed", error.message);
+    }
+  };
+  return (
+    <div>
+      <div>
+        <h1>DashBoard</h1>
+        <button
+          onClick={onLogout}
+          className=" bg-blue-700 rounded-2xl pt-2 px-2 pb-2"
+        >
+          Logout
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default DashBoard;
