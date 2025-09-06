6398e91de26895cb2c54f95f4fda440719dc1b42
Joseph <jotienoochieng@gmail.com>
2025-09-06 10:41:46 +0300

    backend config

---
+++ job-board/src/app/api/users/login/route.ts
@@ -0,0 +1,62 @@
+import User from "@/models/userModels";
+import { connect } from "@/dbConfig/mongodb";
+import { NextRequest, NextResponse } from "next/server";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+connect();
+
+export async function POST(request: NextRequest) {
+  try {
+    const reqBody = await request.json();
+    const { email, password } = reqBody;
+    // console.log(reqBody);
+
+    // check if user exist
+    const user = await User.findOne({ email });
+    if (!user) {
+      return NextResponse.json({
+        error: "user does not exist",
+        status: 400,
+      });
+    }
+
+    //   check if password is correct
+
+    const validPassword = await bcrypt.compare(password, user.password);
+    if (!validPassword) {
+      return NextResponse.json({
+        error: "incorrect password",
+        status: 400,
+      });
+    }
+
+    //   create token data
+
+    const tokenData = {
+      id: user._id,
+      username: user.username,
+      email: user.email,
+    };
+
+    //   create token
+
+    const token = await jwt.sign(tokenData, process.env.TOKEN_SECRET!, {
+      expiresIn: "1d",
+    });
+
+    const response = NextResponse.json({
+      message: "login success",
+      success: true,
+    });
+    //   set token to user cookies
+    response.cookies.set("token", token, { httpOnly: true });
+    return response;
+  } catch (error: any) {
+    return NextResponse.json({
+      error: error.message,
+
+      status: 500,
+    });
+  }
+}
--- job-board/src/app/api/users/signup/route.ts
+++ job-board/src/app/api/users/signup/route.ts
@@ -1,7 +1,49 @@
 import { connect } from "@/dbConfig/mongodb";
 import User from "@/models/userModels";
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+import bcrypt from "bcrypt";
 
 connect();
 
-export async function POST(request: NextRequest) {}
+export async function POST(request: NextRequest) {
+  try {
+    const reqBody = await request.json();
+    const { username, email, password } = reqBody;
+    // console.log(reqBody);
+
+    // checkif user exist
+
+    const user = await User.findOne({ email });
+    if (user) {
+      return NextResponse.json({
+        error: "user already exist",
+        status: 400,
+      });
+    }
+
+    // hash password
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // save user to database
+
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    const saveUser = await newUser.save();
+
+    // console.log(saveUser);
+
+    return NextResponse.json({
+      message: "user created successfully",
+      success: true,
+      saveUser,
+    });
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
