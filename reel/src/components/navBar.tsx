b7e1b56ab0a2ed6ab0c10d8a27f56aadfe7d8d91
Joseph <jotienoochieng@gmail.com>
2026-02-04 14:57:55 +0300

    contact component updates

--- reel/src/components/navBar.tsx
+++ reel/src/components/navBar.tsx
@@ -1,12 +1,44 @@
+import { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 const NavBar = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <div className="fixed top-0 left-0 w-full bg-white shadow-md z-50 ">
-      <div className="flex flex-row justify-end space-x-3.5 px-3 py-4">
-        <Link to="/">Home</Link>
-        <Link to="/contact">Contact</Link>
-        <Link to="/about">About</Link>
+      <div className="flex flex-row justify-between  px-3 py-4">
+        {/* Logo */}
+        <h1 className="text-xl font-bold">My site</h1>
+
+        {/* Desktop Menu */}
+
+        <div className="hidden md:flex space-x-6">
+          <Link to="/">Home</Link>
+          <Link to="/contact">Contact</Link>
+          <Link to="/about">About</Link>
+        </div>
+
+        {/* mobile menu button*/}
+        <div
+          className=" md:hidden cursor-pointer"
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          {isOpen ? <FaTimes size={22} /> : <FaBars size={22} />}
+        </div>
+
+        {isOpen && (
+          <div className=" md:hidden flex flex-col items-center space-y-3 pb-4 bg-white ">
+            <Link to="/" onClick={() => setIsOpen(false)}>
+              Home
+            </Link>
+            <Link to="/contact" onClick={() => setIsOpen(false)}>
+              Contact
+            </Link>
+            <Link to="/about" onClick={() => setIsOpen(false)}>
+              About
+            </Link>
+          </div>
+        )}
       </div>
     </div>
   );
